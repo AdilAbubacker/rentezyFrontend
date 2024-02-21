0d216132ba4b1b9323fca34e20f9bc818e11efbc
Adhil Abubacker <adhilkv313@gmail.com>
2024-02-21 17:20:30 +0530

    tailwing config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = withMT({
     "./index.html",
     "./src/**/**/*.{js,ts,jsx,tsx,html,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,html,mdx}",
+    "./public/fonts/**/*.{woff,woff2,ttf,otf}"
     
   ],
   darkMode: "class",
